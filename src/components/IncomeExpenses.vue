8af7991fcb6a39f68bc7b640478e7a75dba289a0
Bhavesh Chandpa <bhaveshchandpa33@example.com>
2024-09-23 17:31:57 +0530

    Basic_OverView

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money-plus">+$0.00</p>
+        </div>
+        <div>
+            <h4>Expenses</h4>
+            <p id="money-minux" class="money minux">-$0.00</p>
+        </div>
+    </div>
+
+</template>
